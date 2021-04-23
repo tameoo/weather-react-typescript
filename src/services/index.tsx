35cc78c12b795dce48a308d2d65ebdda5eb78519
tameooo <remat01@mail.ru>
2021-04-23 14:25:01 +0600

    fix https request

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,9 +1,10 @@
 
-const apiKey = 'd701466e6f81830ea1596b7e2038a077';
+const apiKeyWeather = 'd701466e6f81830ea1596b7e2038a077';
+const apiKeyPosition = '3ae7c7b2dd1f58c6402da03860317237';
 
 const fetchCurrentWeatherCoords = async ({lat, lon}: {lat: number | null,lon: number | null}) => {
     if(lat && lon){
-        const response = await fetch(`http://pro.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        const response = await fetch(`http://pro.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`)
         
         if(!response.ok){
             throw new Error('Response is not correct to openweather API');
@@ -15,7 +16,7 @@ const fetchCurrentWeatherCoords = async ({lat, lon}: {lat: number | null,lon: nu
 
 const fetchForecastWeatherCoords = async ({lat, lon}: {lat: number | null,lon: number | null}) => {
     if(lat && lon){
-        const response = await fetch(`http://pro.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        const response = await fetch(`http://pro.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`)
 
         if(!response.ok){
             throw new Error('Response is not correct to openweather API');
@@ -27,7 +28,7 @@ const fetchForecastWeatherCoords = async ({lat, lon}: {lat: number | null,lon: n
 
 const getCoordsByName = async (name: string) => {
     if(name){
-        const response = await fetch(`http://api.positionstack.com/v1/forward?access_key=f59c9e1719d53ed060e3c684ab5bb3f7&query=${name}`)
+        const response = await fetch(`https://api.positionstack.com/v1/forward?access_key=${apiKeyPosition}&query=${name}`)
 
         if(!response.ok){
             throw new Error('Response is not correct to openweather API');
